9b367cde2becc1351cdcc36f5e58e53c1313fdd5
David <david@Davids-MacBook-Pro.local>
2022-03-29 17:50:06 -0300

    routes 2

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -14,6 +14,9 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    img: {
+      type: DataTypes.STRING
+    },
     hp: {
       type: DataTypes.INTEGER
     },
--- api/src/routes/pokemon.js
+++ api/src/routes/pokemon.js
@@ -1,28 +1,32 @@
 const { Router } = require('express');
 const axios = require('axios');
 const { Pokemon } = require('../db');
-
+const _LIMIT = 2;
 const router = Router();
 
 router.get('/', async (req, res) => {
-    const pokes = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=2');
+    const pokes = await axios.get('https://pokeapi.co/api/v2/pokemon?limit='+_LIMIT);
     let pokeMap = await Promise.all( pokes.data.results.map(async el => {
         const poke = await axios.get(el.url)
-        let res = {
+        return {
+            id: poke.data.id,
             name: poke.data.name,
-            img: poke.data.sprites.front_default,
-            types: poke.data.types.map(el => el.type.name)
+            img: poke.data.sprites.other['official-artwork'].front_default,
+            types: poke.data.types.map(el => el.type.name),
+            hp: getStat(poke.data.stats,'hp'),
+            attack:getStat(poke.data.stats,'attack') ,
+            defense:getStat(poke.data.stats,'defense'),
+            speed:getStat(poke.data.stats,'speed'),
+            height: poke.data.height,
+            weight: poke.data.weight
         };
-        return res;
+        
     })
     );
     console.log(pokeMap);
     res.send (pokeMap);
 
     
-
-
-
     // axios.get('https://pokeapi.co/api/v2/pokemon?limit=2').
     //     then((response) => res.send(response.data.results.map( el => {
     //         axios.get(el.url)
@@ -42,9 +46,22 @@ router.get('/', async (req, res) => {
 
 router.get('/')
 
-
-
+// Trae el valor del stat del arreglo de 'stats' cuyo nombre es 'name'
+function getStat(stats, name) {
+    for( let i = 0; i < stats.length; i++) {
+        if (stats[i].stat.name === name) return stats[i].base_stat;
+    }
+    return 0;
+}
 
 module.exports = router;
 
 
+// ID (Número de Pokemon) * : No puede ser un ID de un pokemon ya existente en la API pokeapi
+// Nombre * (name)
+// Vida     (poke.stats.[x].stat.name = 'hp')
+// Fuerza   (poke.stats.[x].stat.name = 'attack')
+// Defensa  (poke.stats.[x].stat.name = 'defense')
+// Velocidad  (poke.stats.[x].stat.name = 'speed')
+// Altura     poke.height
+// Peso       poke.weight
