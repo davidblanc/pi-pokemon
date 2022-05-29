3136435610711979f9fef7638b12e1f3ba246e7b
David Blanc <davidblanc@gmail.com>
2022-05-29 20:30:54 -0300

    edit index.js to herku

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const axios = require('axios')
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 }).then( async () => {
